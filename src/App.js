50426f8e59e11035c291d22417220016df678d80
Shahnawaz Idariya <shahnawazidariya52@gmail.com>
2023-05-30 19:50:57 -0700

    [Add] Added table for tickets

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import DefaultLayout from './layout/DefaultLayout';
 
 import EntryPage from './pages/entry/EntryPage';
+import Dashboard from './pages/dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
       {/* <EntryPage /> */}
       <DefaultLayout>
-          send page component
+          <Dashboard />
         </DefaultLayout>
     </div>
   );
---
+++ src/components/TicketTable/TicketTable.js
@@ -0,0 +1,36 @@
+import { Table } from "react-bootstrap";
+
+const TicketTable = ({tickets}) => {
+    return (
+        <div>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Subjects</th>
+                        <th>Status</th>
+                        <th>Opened Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tickets.length ? tickets.map((row) => (
+                        <tr key={row.id}>
+                            <td>{row.id}</td>
+                            <td>{row.subject}</td>
+                            <td>{row.status}</td>
+                            <td>{row.addedAt}</td>
+                        </tr>
+                    )) :
+                    <tr>
+                        <td colSpan="4" className="text-center">No ticket show</td>
+                    </tr>
+                    }
+
+
+                </tbody>
+            </Table>
+        </div>
+    )
+}
+
+export default TicketTable;
